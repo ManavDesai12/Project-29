8e5983cfb7337ddb6d456fe7f5cd44c40acf6648
ManavDesai12 <72940151+ManavDesai12@users.noreply.github.com>
2021-03-03 18:59:32 -0800

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,31 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness:0.01,
+            length: 20
+        }
+        this.pointB = pointB
+        this.body = Constraint.create(options);
+       
+        World.add(world, this.body);
+    }
+
+    fly(){
+        this.body.bodyA = null;
+    }
+
+    display(){
+    
+        if(this.body.bodyA){
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            stroke(48, 22, 8);
+            strokeWeight(4);
+            
+           
+        }
+    }
+    
+}
